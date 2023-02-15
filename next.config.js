93d90aa4c073266eaee3f0bfb0acaa2253821cb5
Azin <aazadi@gmail.com>
2023-02-15 16:54:21 +0100

    clean config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path')
+// const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
-  webpack: (config) => {
-    // understand absolute paths on importing
-    config.resolve.modules.push(path.resolve('./'));
-    return config
-  },
+  // webpack: (config) => {
+  //   // understand absolute paths on importing
+  //   config.resolve.modules.push(path.resolve('./'));
+  //   return config
+  // },
   images: {
     formats: ['image/avif', 'image/webp'],
     // loader: "custom",
@@ -16,22 +16,22 @@ const nextConfig = {
     // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   // transpilePackages: ["next-image-export-optimizer"],
-  env: {
-    nextImageExportOptimizer_imageFolderPath: "images",
-    nextImageExportOptimizer_exportFolderPath: "out",
-    nextImageExportOptimizer_quality: 80,
-    nextImageExportOptimizer_storePicturesInWEBP: true,
-    // nextImageExportOptimizer_exportFolderPath: "nextImageExportOptimizer",
-
-    // If you do not want to use blurry placeholder images, then you can set
-    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
-    // `placeholder="empty"` to all <ExportedImage> components.
-    //
-    // If nextImageExportOptimizer_generateAndUseBlurImages is false and you
-    // forget to set `placeholder="empty"`, you'll see 404 errors for the missing
-    // placeholder images in the console.
-    nextImageExportOptimizer_generateAndUseBlurImages: true,
-  },
+  // env: {
+  //   nextImageExportOptimizer_imageFolderPath: "images",
+  //   nextImageExportOptimizer_exportFolderPath: "out",
+  //   nextImageExportOptimizer_quality: 80,
+  //   nextImageExportOptimizer_storePicturesInWEBP: true,
+  //   // nextImageExportOptimizer_exportFolderPath: "nextImageExportOptimizer",
+  //
+  //   // If you do not want to use blurry placeholder images, then you can set
+  //   // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+  //   // `placeholder="empty"` to all <ExportedImage> components.
+  //   //
+  //   // If nextImageExportOptimizer_generateAndUseBlurImages is false and you
+  //   // forget to set `placeholder="empty"`, you'll see 404 errors for the missing
+  //   // placeholder images in the console.
+  //   nextImageExportOptimizer_generateAndUseBlurImages: true,
+  // },
 
 }
 
